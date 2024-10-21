f44a60432f59621d3ec9e61bb09d2eebcf64b78c
uzzyboy <62774773+uzzyboy@users.noreply.github.com>
2024-10-21 10:36:24 +0200

    update

--- assets/js/quick-reserve.js
+++ assets/js/quick-reserve.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    
+
+    let lang = document.getElementById('#lingua_int').value;
+
     if(typeof(document.getElementById("dario")) != 'undefined' && document.getElementById("dario") != null){
         const dario = new Dario('#dario', {
             classes: 'dario--mod',
             inline: false,
             range: true,
             minDate: new Date(),
-            lang: 'ita',
+            lang: lang,
             onSelect: (result) => {
                 //console.log(result);
                 document.getElementById('data-arrivo').innerHTML = result.startDate.date+"&nbsp;"+result.startMonthShort+"&nbsp;"+result.startDate.year;
